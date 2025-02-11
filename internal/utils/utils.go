8d0c452566f2689165bb9454a32164753d1ad9dc
dima <kozhevin.dima@gmail.com>
2025-02-12 02:08:19 +0300

    white_users.json for tests

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 func AppendArgs(target []string, key string, value string) []string {
 	if value != "" {
 		target = append(target, key)
@@ -7,4 +13,24 @@ func AppendArgs(target []string, key string, value string) []string {
 	}
 
 	return target
+}
+
+func WrapError(err error, msg string) error {
+	return fmt.Errorf("%s: %w ", msg, err)
+}
+
+func ReadFile(path string) ([]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, WrapError(err, "failed to read file")
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+	result, err := io.ReadAll(file)
+	if err != nil {
+		return nil, WrapError(err, "failed to read file")
+	}
+
+	return result, nil
 }
--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,4 +14,33 @@ func TestAppendArgs(t *testing.T) {
 	want := []string{"a", "b"}
 	got := AppendArgs(args, "a", "b")
 	assert.Equal(t, want, got)
+}
+
+func TestReadFile(t *testing.T) {
+	data, err := ReadFile(GetWD() + "/test/testdata/white_users.json")
+	assert.NoError(t, err)
+	assert.NotNil(t, data)
+}
+
+func TestReadFileNoPath(t *testing.T) {
+	b, err := ReadFile("")
+	assert.Nil(t, b)
+	assert.Error(t, err)
+}
+
+var workDir = ""
+
+func GetWD() string {
+	if workDir != "" {
+		return workDir
+	}
+	wDirTemp, _ := os.Getwd()
+
+	for !strings.HasSuffix(wDirTemp, "internal") {
+		wDirTemp = filepath.Dir(wDirTemp)
+	}
+	wDirTemp = filepath.Dir(wDirTemp)
+	workDir = wDirTemp
+
+	return workDir
 }
