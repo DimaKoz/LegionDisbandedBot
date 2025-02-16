7f24e1a6b106e8702dc8878b40d45c4550098c8e
dima <kozhevin.dima@gmail.com>
2025-02-16 17:17:36 +0300

    loading cached telegram users + tests

--- internal/repository/tg_user_storage.go
+++ internal/repository/tg_user_storage.go
@@ -1,9 +1,14 @@
 package repository
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
 	"sync"
 
 	"github.com/DimaKoz/LegionDisbandedBot/internal/model/user"
+	"go.uber.org/zap"
 )
 
 var (
@@ -44,4 +49,37 @@ func GetTelegramUser(key string) (*user.CachedTelegramUser, error) {
 	}
 
 	return nil, repositoryError(errNotFoundTelegramUser, key)
+}
+
+func LoadCachedTelegramUser(filepath string) error {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return fmt.Errorf("TelegramUserStorage: can't read '%s' file with error: %w", filepath, err)
+	}
+	defer func(file *os.File) {
+		if err = file.Close(); err != nil {
+			zap.S().Warnf("TelegramUserStorage: can't close the file: %s with err: %s \n", filepath, err)
+		}
+	}(file)
+	var tgUsers []user.CachedTelegramUser
+
+	const bufferSize = 128
+	tgUserReader := bufio.NewReaderSize(file, bufferSize)
+	if err = json.NewDecoder(tgUserReader).Decode(&tgUsers); err != nil {
+		return fmt.Errorf("TelegramUserStorage: failed to parse json with error: %w", err)
+	}
+
+	if len(tgUsers) == 0 {
+		return errNoSavedData
+	}
+
+	tgUserStorageSync.Lock()
+	defer tgUserStorageSync.Unlock()
+	for _, v := range tgUsers {
+		tgUserStorage.storage[v.Nickname] = v
+	}
+
+	zap.S().Infof("TelegramUserStorage repository: loaded: %d \n", len(tgUsers))
+
+	return nil
 }
--- internal/repository/tg_user_storage_test.go
+++ internal/repository/tg_user_storage_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/DimaKoz/LegionDisbandedBot/internal/model/user"
+	"github.com/DimaKoz/LegionDisbandedBot/internal/testutils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -47,6 +48,69 @@ func TestAddGetGetTelegramUser(t *testing.T) {
 			got, err = GetTelegramUser(tUnit.args.key)
 			require.Error(t, err)
 			assert.Nil(t, got)
+		})
+	}
+}
+
+func TestLoadCachedTelegramUser(t *testing.T) {
+	type args struct {
+		filepath string
+	}
+	wdir := testutils.GetWD()
+	tests := []struct {
+		name             string
+		args             args
+		isWantErr        bool
+		wantTelegramUser *user.CachedTelegramUser
+		expectedNumbers  int
+	}{
+		{
+			name:             "empty path",
+			args:             args{filepath: ""},
+			wantTelegramUser: nil, isWantErr: true, expectedNumbers: 0,
+		},
+		{
+			name: "ok",
+			args: args{filepath: wdir + "/test/testdata/cached_users.json"},
+			wantTelegramUser: &user.CachedTelegramUser{
+				ID: 123, Nickname: "@Nickname1", FirstName: "FirstName1",
+				LastName: "LastName1", IsBot: true, IsBanned: true,
+			},
+			isWantErr: false, expectedNumbers: 2,
+		},
+		{
+			name: "bad json",
+			args: args{
+				filepath: wdir + "/test/testdata/white_users_bad.json",
+			}, wantTelegramUser: nil, isWantErr: true, expectedNumbers: 0,
+		},
+		{
+			name: "no tg users",
+			args: args{
+				filepath: wdir + "/test/testdata/white_users_zero.json",
+			}, wantTelegramUser: nil, isWantErr: true, expectedNumbers: 0,
+		},
+	}
+	for _, tCase := range tests {
+		tUnit := tCase
+		t.Run(tUnit.name, func(t *testing.T) {
+			tgUserStorageOrig := tgUserStorage
+			tgUserStorage = TelegramUserStorage{
+				storage: make(map[string]user.CachedTelegramUser),
+			}
+			t.Cleanup(func() { tgUserStorage = tgUserStorageOrig })
+			err := LoadCachedTelegramUser(tUnit.args.filepath)
+			if tCase.isWantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tUnit.expectedNumbers, len(tgUserStorage.storage), "Wrong numbers of telegram users")
+			if tUnit.wantTelegramUser != nil {
+				got, err := GetTelegramUser(tUnit.wantTelegramUser.Nickname)
+				assert.NoError(t, err)
+				assert.Equal(t, *tUnit.wantTelegramUser, *got)
+			}
 		})
 	}
 }
